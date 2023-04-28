fbdbc01dd2a5c21b1001210e86eee468ce613bd5
Rahul Kumar <rahulloni000@gmail.com>
2023-04-28 07:09:20 +0545

    menu com

--- app/src/main/java/com/example/card_design/MainActivity.java
+++ app/src/main/java/com/example/card_design/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.card_design;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_example,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(this, "Click Me ", Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()){
+            case R.id.item1:
+                return true;
+            case R.id.item2:
+                Intent intent=new Intent(getApplicationContext(),second.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
