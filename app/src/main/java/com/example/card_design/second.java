e5cff96a16357d3c40ae87a3f9b113c080bc563c
Rahul Kumar <rahulloni000@gmail.com>
2023-04-30 06:59:28 +0545

    alert dialog

--- app/src/main/java/com/example/card_design/second.java
+++ app/src/main/java/com/example/card_design/second.java
@@ -1,8 +1,12 @@
 package com.example.card_design;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
 public class second extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class second extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    public void delete(View view) {
+        AlertDialog.Builder builder=new AlertDialog.Builder(second.this);
+        builder.setMessage("DO you want to delete?");
+        builder.setTitle("Alert!!!");
+        builder.setCancelable(true);
+        builder.setPositiveButton("yes",(DialogInterface.OnClickListener)(dialog,which)->{
+           finish();
+        });
+        builder.setPositiveButton("no",(DialogInterface.OnClickListener)(dialog,which)->{
+          dialog.cancel();
+        });
+        AlertDialog alertDialog=builder.create();
+        alertDialog.show();
     }
 }
